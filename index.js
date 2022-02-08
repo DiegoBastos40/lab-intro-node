312ba86997ea821f919b4effacbadd28611b4f6a
DiegoBastos40 <diegobastosherval@gmail.com>
2022-02-08 14:20:47 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,69 @@
 class SortedList {
-  constructor() {}
 
-  add(item) {}
+  
+  constructor() {
+   
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  add(item) {
+    
+      this.items.push(item);
+      this.length = this.items.length ;
+      return this.items.sort((a, b) => a - b);
+    }
+  
 
-  max() {}
+  get(pos) {
+    if(this.items.indexOf(pos) === -1){
+      throw new Error("Something went wrong");
+          
+      }
+      else {
+        return this.items[pos];
+      }
 
-  min() {}
+  }
 
-  sum() {}
+  max() {
+    if(this.items.length === 0){
+      throw  new  Error ( 'EmptySortedList' ) ;
+  } else {
+    return Math.max(...this.items);
+  }
+     
+  }
 
-  avg() {}
+  min() {
+    if(this.items.length === 0){
+      throw  new  Error ( 'EmptySortedList' ) ;
+  } else {
+    return Math.min(...this.items);
+  }
+  }
+
+  sum() {
+
+    if(this.length === 0){
+      return 0 ;
+    }else {
+      return this.items.reduce((acc,current)=> acc + current)
+    
+    }
+    
+   }
+
+    
+
+
+  avg() {
+    if(this.length === 0){
+    throw  new  Error ( 'error' )
+    }else {
+      return avg = this.acc / this.length
+    }
+}
 }
 
 module.exports = SortedList;
